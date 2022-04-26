ca3d6ecfce6a3238fe83a0dda994e10dd6ca0aaf
Toly <teran4work@gmail.com>
2022-04-26 18:20:54 +0300

    Добавлен логотип

--- components/sections/header.js
+++ components/sections/header.js
@@ -10,17 +10,11 @@ const HeaderIS = styled.header`
     height: ${HeaderHeight}px;
     display: flex;
 `;
-const TextIS = styled.div`
-    text-transform: uppercase;
-    color: #fff;
-    text-shadow: 1px 1px 2px #000, 1px 1px 6px #000, 1px 1px 10px #000;
-    margin: 0px;
-`;
 
 export const Header = () => {
     return (
         <HeaderIS>
-            <TextIS>Euro Filter</TextIS>
+            <img src="/euro_filter_color.svg" alt="Euro Filter"/>
         </HeaderIS>
     );
 };
